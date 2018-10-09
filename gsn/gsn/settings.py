aa4dc5d6a0b12c4a67f55b0f92f2ba56e137aad8
MTempleman <huntertempleman@gmail.com>
2018-10-09 17:40:58 -0600

    Added global staticfiles directory for django.

--- gsn/gsn/settings.py
+++ gsn/gsn/settings.py
@@ -119,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+#Extra place for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
