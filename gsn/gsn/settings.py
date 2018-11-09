21ead4eda918dee895d82b897c79a1383eba0cc8
MTempleman <huntertempleman@gmail.com>
2018-11-09 11:04:07 -0700

    set Debug to True to avoid confusion during development

--- gsn/gsn/settings.py
+++ gsn/gsn/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'gsn.gsnsecrets.gsn_django_secretkey'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
