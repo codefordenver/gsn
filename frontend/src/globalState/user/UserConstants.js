f6ce35b76e6d513f99357aac4a3cf9f659616190
Zachary Rose <zachary.f.rose@gmail.com>
2019-01-07 20:28:23 -0700

    add missing user constants

---
+++ frontend/src/globalState/user/UserConstants.js
@@ -0,0 +1,5 @@
+export SET_AUTH_TOKEN = 'user/SET_AUTH_TOKEN';
+export CLEAR_AUTH = 'user/CLEAR_AUTH';
+export AUTH_REQUEST = 'user/AUTH_REQUEST';
+export AUTH_SUCCESS = 'user/AUTH_SUCCESS';
+export AUTH_ERROR = 'user/AUTH_ERROR';
