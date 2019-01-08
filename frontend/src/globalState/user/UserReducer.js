8fba9e556f33a5078d252d461f5e3e023e1ee818
Zack Rose <zachary.f.rose@gmail.com>
2019-01-07 22:12:30 -0700

    Change Reducer to Immutable.JS and switch statement logic

--- frontend/src/globalState/user/UserReducer.js
+++ frontend/src/globalState/user/UserReducer.js
@@ -1,3 +1,4 @@
+import {fromJS} from 'immutable';
 import {
   SET_AUTH_TOKEN,
   CLEAR_AUTH,
@@ -6,42 +7,38 @@ import {
   AUTH_ERROR,
 } from './UserConstants';
 
-const initialState = {
+const initialState = fromJS({
   authToken: null,
   isLoggedIn: false,
-  userName: null,
-  laoding: false,
+  username: null,
+  loading: false,
   error: null
-};
+});
 
 export default function reducer(state = initialState, action) {
-  if (action.type === SET_AUTH_TOKEN) {
-    return Object.assign({}, state, {
-      authToken: action.authToken
-    });
-  } else if (action.type === CLEAR_AUTH) {
-    return Object.assign({}, state, {
-      authToken: null,
-      userName: null,
-      isLoggedIn: false
-    });
-  } else if (action.type === AUTH_REQUEST) {
-    return Object.assign({}, state, {
-      loading: true,
-      error: null
-    });
-  } else if (action.type === AUTH_SUCCESS) {
-    return Object.assign({}, state, {
-      loading: false,
-      // TODO add in userName
-      // TODO add in authToken
-      isLoggedIn: true
-    });
-  } else if (action.type === AUTH_ERROR) {
-    return Object.assign({}, state, {
-      loading: false,
-      error: action.error
-    });
+
+  const {type, payload} = action;
+
+  switch (type) {
+    case SET_AUTH_TOKEN:
+      return state.set('authToken', payload);
+    case CLEAR_AUTH:
+      return state.set('authToken', null)
+                  .set('userName', null)
+                  .set('isLoggedIn', false);
+    case AUTH_REQUEST:
+      return state.set('loading', true)
+                  .set('error', null);
+    case AUTH_SUCCESS:
+      return state.set('loading', false)
+                  .set('isLoggedIn', true)
+                  .set('username', payload.username)
+                  .set('authToken', payload.authToken);
+    case AUTH_ERROR:
+      return state.set('loading', false)
+                  .set('error', payload);
+    default: return state;
+
   }
-  return state;
+  
 }
