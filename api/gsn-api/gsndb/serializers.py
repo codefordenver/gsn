cac29df9120cb38f646c0a6bab98a415652dfea3
Ivan Anich <ivanpiace123@yahoo.com>
2019-02-09 17:25:39 -0700

    update serializers with school, allstudent, and studentdetail serializers.

--- api/gsn-api/gsndb/serializers.py
+++ api/gsn-api/gsndb/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from gsndb.models import District
+from gsndb.models import District, School, Student, Course, Grade, Attendance, Behavior, Referral
 
 class DistrictSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,43 @@ class DistrictSerializer(serializers.ModelSerializer):
             "city",
             "state",
             "name")
+
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = (
+            "id",
+            "district",
+            "name",
+        )
+
+class AllStudentSerializer(serializers.ModelSerializer):
+    current_school = SchoolSerializer(read_only = True)
+
+    class Meta:
+        model = student
+        fields = (
+        "first_name",
+        "last_name",
+        "middle_name",
+        "current_school",
+        "birth_date",
+        )
+
+class StudentDetailSerializer(serializers.ModelSerializer):
+    school = SchoolSerializer(read_only = True)
+
+    class Meta:
+        model = student
+        fields = (
+        "first_name",
+        "last_name",
+        "middle_name",
+        "current_school",
+        "birth_date",
+        "gender",
+        "grade_year",
+        "program",
+        "reason_in_program",
+        "state_id",
+        )
