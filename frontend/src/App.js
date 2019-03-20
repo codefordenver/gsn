b9d206888aa5431fdec44600d919ba19a5832160
Phillip Roberts <piroberts@gatech.edu>
2019-03-19 21:22:10 -0600

    cleaned up some files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,27 +7,20 @@ import { MuiThemeProvider } from '@material-ui/core';
 import theme from './utils/theme';
 import Routes from './components/Routes';
 
-const loggedInNav = [
-  { key: 'navitem1', path: '/', text: 'Home' },
-  { key: 'navitem2', path: '/students', text: 'All Students' },
-  { key: 'navitem3', path: '/districts', text: 'All Districts' },
-];
+// const loggedInNav = [
+//   { key: 'navitem1', path: '/', text: 'Home' },
+//   { key: 'navitem2', path: '/students', text: 'All Students' },
+//   { key: 'navitem3', path: '/districts', text: 'All Districts' },
+// ];
 
 function App(props) {
-  const { setUserState } = props;
+  const { loading, setUserState } = props;
 
   useEffect(() => {
     setUserState();
   }, []);
 
-  const {
-    username, isLoggedIn, loading, logOut,
-  } = props;
   if (loading) return <h1>Loading...</h1>;
-
-
-  const logOutItem = { key: 'navitem10', action: logOut, text: 'Log Out' };
-  const navItems = [...loggedInNav, logOutItem];
 
   return (
       <MuiThemeProvider theme={theme}>
@@ -50,5 +43,4 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, {
   setUserState: userActions.setUserState,
-  logOut: userActions.logOut,
 })(App);
