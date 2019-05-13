da63a202aa1e40518447dc6b2f6ad2c68056843e
hannahkamundson <hannahkamundson@gmail.com>
2019-05-12 20:55:10 -0600

    Added testing examples for models for somebody else to finish off

--- api/gsn_api/gsndb/tests/test_models.py
+++ api/gsn_api/gsndb/tests/test_models.py
@@ -7,35 +7,56 @@ print("test_models")
 # test minimum number of fields that need to be entered
 from django.test import TestCase
 from gsndb.models import Note, District
+from django.contrib.auth.models import User
 
 
 class DistrictModelTest(TestCase):
     def test_create_instance_minimum_fields(self):
-        newDistrict = District.objects.create(name = 'Denver Public Schools', state = 'CO', city = 'Denver', code = 'DPS')
+        newDistrict = District.objects.create()
 
-'''    create_instance_all_fields(self):
-
-    get_instance_all_fields(self):
-
-    update_instance(self):
-
-    bad_field_inputs(self):
-
-    add_note(self):
-
-    delete_instance(self):
-''' 
-
+    def test_bad_field_inputs(self):
+        print("Need to addresss")
+        #newDistrict = District.objects.create(state = 'Colorado')
 
 '''
-class District(models.Model):
+do this for the remaining models
+we want to make sure that the minimum fields we want work for all models
+we also want to test any strange inputs to ensure it works out okay
 
-    state = models.CharField(
-        max_length=2,
-        blank = True,
-        choices = STATE_CHOICES,
-    )
-    city = models.CharField(max_length=50, blank=True)
-    code = models.CharField(max_length=10, blank=True)
-    name = models.CharField(max_length=100, blank=True)
-    notes = GenericRelation(Note)'''
+Remaining Models:
+School
+Program
+Student
+Course
+Calendar
+Behavior
+Grade
+Attendance
+'''
+
+class NoteModelTest(TestCase):
+    def setUp(self):
+        self.newUser = User.objects.create_user(
+            username='Hannah',
+            email='hannahkamundson@beatles.com',
+            password='glass onion')
+        
+    def test_district_note(self):
+        newDistrict = District.objects.create(name = 'Denver Public Schools')
+        newNote = Note.objects.create(user = self.newUser, text = 'This is a district', content_object = newDistrict)
+
+'''
+under this NoteModelTest do the same as test_district_note for the following  models:
+
+Remaining Models:
+School
+Program
+Student
+Course
+Calendar
+Behavior
+Grade
+Attendance
+Referral
+Bookmark
+'''
