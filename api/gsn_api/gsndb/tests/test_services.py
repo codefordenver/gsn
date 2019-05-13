2886890eb68cccdd2dc0a63312c9c27ed3fc443b
hannahkamundson <hannahkamundson@gmail.com>
2019-05-12 20:55:42 -0600

    Add comments for what needs to be tested for the csv parser

--- api/gsn_api/gsndb/tests/test_services.py
+++ api/gsn_api/gsndb/tests/test_services.py
@@ -1,3 +1,4 @@
 print("test_services")
 
-#test duplicates
+#test duplicates to ensure when all fields aren't inputted that it doesn't create a duplicate
+#for example if Hannah Amundson is put in but Hannah Kristin Amundson isn't will it create a duplicate? need to discuss
