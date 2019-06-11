0362092c050fb595a6f7d0b31ee7ac7435f1725f
Ivan Anich <ivanpiace123@yahoo.com>
2019-06-11 09:51:15 -0600

    organized and trimmed unused and redundant code.

--- api/gsn_api/gsndb/views.py
+++ api/gsn_api/gsndb/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from gsndb.models import Program, District, School, Student, Course, Calendar, Grade, Behavior, Attendance, Referral, Note, Bookmark, Program
 from gsndb.serializers import ProgramSerializer, ProgramDetailSerializer, CourseDetailSerializer, SchoolDetailSerializer, StudentDetailSerializer,DistrictSerializer, DistrictDetailSerializer, SchoolSerializer, StudentSerializer, CourseSerializer, CalendarSerializer, GradeSerializer, BehaviorSerializer, AttendanceSerializer, ReferralSerializer, NoteSerializer, BookmarkSerializer, NestedSchoolSerializer, NestedStudentSerializer, NestedProgramSerializer, MyStudentsSerializer
@@ -32,6 +27,15 @@ class ProgramList(generics.ListCreateAPIView):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
 
+class NoteList(generics.ListCreateAPIView):
+    #returns all notes for anything
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+class BookmarkList(generics.ListCreateAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+
 #Detail views
 class DistrictDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = District.objects.all()
@@ -53,105 +57,11 @@ class ProgramDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Program.objects.all()
     serializer_class = ProgramDetailSerializer
 
-
-
-#Other
-class NoteByObject(APIView):
-
-    def get(self, request, pk, objType):
-        
-        contType = ContentType.objects.get(app_label = "gsndb", model = objType).id
-        notes = Note.objects.filter(content_type = contType, object_id = pk)
-        data = NoteSerializer(notes, many = True).data
-        
-        return Response(data)
-
-
-
-
-
-class DistrictDetail(generics.ListCreateAPIView):
-    queryset = District.objects.all()
-    serializer_class = DistrictDetailSerializer
-
-
-
-class CalendarList(generics.ListCreateAPIView):
-    queryset = Calendar.objects.all()
-    serializer_class = CalendarSerializer
-
-class CalendarDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Calendar.objects.all()
-    serializer_class = CalendarSerializer
-
-
-class MyStudentsList(generics.ListCreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = MyStudentsSerializer
-
-class GradeList(generics.ListCreateAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-
-class GradeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-
-class GradeForStudent(APIView):
-    """# http POST http://127.0.0.1:8000/gsndb/student/someviewendpoint requestParameter="requestValue"""
-    def get(self, request, pk, format = None):
-        student_obj = Student.objects.filter(pk = pk)
-        serializer = GradeForStudentSerializer(student_obj, many = True)
-        return Response(serializer.data)
-
-
-class BehaviorList(generics.ListCreateAPIView):
-    queryset = Behavior.objects.all()
-    serializer_class = BehaviorSerializer
-
-class BehaviorDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Behavior.objects.all()
-    serializer_class = BehaviorSerializer
-
-
-class AttendanceList(generics.ListCreateAPIView):
-    queryset = Attendance.objects.all()
-    serializer_class = AttendanceSerializer
-
-class AttendanceDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Attendance.objects.all()
-    serializer_class = AttendanceSerializer
-
-
-class ReferralList(generics.ListCreateAPIView):
-    queryset = Referral.objects.all()
-    serializer_class = ReferralSerializer
-
-class ReferralDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Referral.objects.all()
-    serializer_class = ReferralSerializer
-
-class NoteList(generics.ListCreateAPIView):
-    #returns all notes for anything
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-
-class BookmarkList(generics.ListCreateAPIView):
-    queryset = Bookmark.objects.all()
-    serializer_class = BookmarkSerializer
-
 class BookmarkDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
 
-'''
-class SchoolFakeInfo(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSchoolSerializer
-'''
-
-
-
+#Info views
 class SchoolInfo(APIView):
 
     def get(self, request, pk, grade = False, course = False, behavior = False, referral = False, attendance = False, format = None):
@@ -166,14 +76,14 @@ class SchoolInfo(APIView):
             serializer = NestedSchoolSerializer(school_obj, many = True, context = {"getReferral": True})
         elif self.kwargs.get("course"):
             serializer = NestedSchoolSerializer(school_obj, many = True, context = {"getCourse": True})
-        
+
         return Response(serializer.data)
 
 class StudentInfo(APIView):
 
     def get(self, request, pk, grade = False, course = False, behavior = False, referral = False, attendance = False, format = None):
         student_obj = Student.objects.filter(pk = pk)
-        if self.kwargs.get("grade"):    
+        if self.kwargs.get("grade"):
             serializer = NestedStudentSerializer(student_obj, many = True, context = {"getGrades": True})
         elif self.kwargs.get("attendance"):
             serializer = NestedStudentSerializer(student_obj, many = True, context = {"getAttendance": True})
@@ -198,10 +108,17 @@ class ProgramInfo(APIView):
             serializer = NestedProgramSerializer(program_obj, many = True, context = {"getReferral": True})
         elif self.kwargs.get("course"):
             serializer = NestedProgramSerializer(program_obj, many = True, context = {"getCourse": True})
-        
+
         return Response(serializer.data)
 
 
+#Other
+class NoteByObject(APIView):
 
+    def get(self, request, pk, objType):
 
+        contType = ContentType.objects.get(app_label = "gsndb", model = objType).id
+        notes = Note.objects.filter(content_type = contType, object_id = pk)
+        data = NoteSerializer(notes, many = True).data
 
+        return Response(data)
