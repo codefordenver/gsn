9bb7c00ff7fee43c4dabdf02b7d14b1c3a03bdcd
hannahkamundson <hannahkamundson@gmail.com>
2019-06-12 19:56:20 -0600

    Added my student queryset

--- api/gsn_api/gsndb/filterSecurity.py
+++ api/gsn_api/gsndb/filterSecurity.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import User
-from gsndb.models import StudentUserHasAccess
+from gsndb.models import StudentUserHasAccess, MyStudents
+
+allAccess = "all"
+myAccess = "my"
 
 user = User.objects.first()
 accessibleStudents = StudentUserHasAccess.objects.filter(user=user).values('student')
+
+myStudentsPk = MyStudents.objects.values('studentUserHasAccess')
+myStudents = StudentUserHasAccess.objects.filter(user=user, pk__in=myStudentsPk).values('student')
