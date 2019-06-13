063ff20208cb9fd742eb032c8fe7d19aba6938f9
hannahkamundson <hannahkamundson@gmail.com>
2019-06-12 20:10:31 -0600

    Added my programs filter

--- api/gsn_api/gsndb/filterSecurity.py
+++ api/gsn_api/gsndb/filterSecurity.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
-from gsndb.models import StudentUserHasAccess, MyStudents
+from gsndb.models import StudentUserHasAccess, MyStudents, MyPrograms
 
 allAccess = "all"
 myAccess = "my"
 
 user = User.objects.first()
+
 accessibleStudents = StudentUserHasAccess.objects.filter(user=user).values('student')
 
 myStudentsPk = MyStudents.objects.values('studentUserHasAccess')
 myStudents = StudentUserHasAccess.objects.filter(user=user, pk__in=myStudentsPk).values('student')
+
+myPrograms = MyPrograms.objects.filter(user=user).values('program')
