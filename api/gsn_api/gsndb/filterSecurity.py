1e4b623256d492b006c2a7149cf6862deae8b236
hannahkamundson <hannahkamundson@gmail.com>
2019-06-13 20:10:29 -0600

    modified python styles & changed it to have correct filtering for my for school/course/district

--- api/gsn_api/gsndb/filterSecurity.py
+++ api/gsn_api/gsndb/filterSecurity.py
@@ -1,11 +1,44 @@
 from django.contrib.auth.models import User
-from gsndb.models import StudentUserHasAccess, MyStudents
+from gsndb.models import StudentUserHasAccess, MyStudents, Student, School, Grade
 
-allAccess = "all"
-myAccess = "my"
 
-user = User.objects.first()
-accessibleStudents = StudentUserHasAccess.objects.filter(user=user).values('student')
 
-myStudentsPk = MyStudents.objects.values('studentUserHasAccess')
-myStudents = StudentUserHasAccess.objects.filter(user=user, pk__in=myStudentsPk).values('student')
+class FilterSecurity():
+    all_access = "all"
+    my_access = "my"
+
+    def __init__(self):
+        self.user = User.objects.first()
+
+    def get_all_access(self):
+        return self.all_access
+
+    def get_my_access(self):
+        return self.my_access
+
+    def get_accessible_students(self):
+        accessible_students = StudentUserHasAccess.objects.filter(user=self.user).values('student')
+        return accessible_students
+
+    def get_my_students(self):
+        my_students_pk = MyStudents.objects.values('student_user_has_access')
+        my_students = StudentUserHasAccess.objects.filter(user=self.user, pk__in=my_students_pk).values('student')
+        return my_students
+
+    def get_my_districts(self):
+        school_list = self.get_my_schools()
+        district_list = School.objects.filter(pk__in=school_list).values('district')
+        return district_list
+        
+
+    def get_my_schools(self):
+        student_list = self.get_my_students()
+        school_list = Student.objects.filter(pk__in=student_list).values('current_school')
+        return school_list
+
+    def get_my_courses(self):
+        student_list = self.get_my_students()
+        course_list = Grade.objects.filter(student__in=student_list).values('course')
+        return course_list
+
+
