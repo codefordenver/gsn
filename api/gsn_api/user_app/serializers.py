d903e4dd06ebc443cc1ed01d973cb76b7507eae7
Hannah Amundson <hannahkamundson@gmail.com>
2019-07-04 20:11:32 -0600

    Added a serializer for the registration secret key

--- api/gsn_api/user_app/serializers.py
+++ api/gsn_api/user_app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
+from user_app.models import SpecialKeyLog
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -34,3 +35,8 @@ class UserSerializerWithToken(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('token', 'username', 'password')
+
+class SpecialKeyLogList(serializers.ModelSerializer):
+    class Meta:
+        model = SpecialKeyLog
+        fields = ('special_key', 'created', 'user_that_create_request',)
